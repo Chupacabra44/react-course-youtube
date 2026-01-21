bd3f67ecb92568546c690feaa2b865e6285f91e0
Sanja Daic <sanja.daic86@gmail.com>
2026-01-21 08:10:39 +0100

    fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,8 +1,13 @@
 import Header from "../components/Header";
 import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
+import { data } from "react-router";
 
 const HomePage = () => {
+  fetch("http://localhost:3000/api/products").then((response) =>
+    response.json().then((data) => console.log(data)),
+  );
+
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="/home-favicon.png" />
