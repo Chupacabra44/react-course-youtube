1de3e9c83bade9eb694fc2d6fa18eee38f363c5b
Sanja Daic <sanja.daic86@gmail.com>
2026-01-21 08:41:38 +0100

    use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,12 +1,12 @@
+import axios from "axios";
 import Header from "../components/Header";
 import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
-import { data } from "react-router";
 
 const HomePage = () => {
-  fetch("http://localhost:3000/api/products").then((response) =>
-    response.json().then((data) => console.log(data)),
-  );
+  axios.get("http://localhost:3000/api/products").then((response) => {
+    console.log(response.data);
+  });
 
   return (
     <>
