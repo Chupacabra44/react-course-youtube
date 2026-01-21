a8a2ec4e8009433bbe45a09bb3d3f8f02f11ab8a
Sanja Daic <sanja.daic86@gmail.com>
2026-01-21 14:37:33 +0100

    separate into smaller components

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router";
 import Header from "../components/Header";
 import "./TrackingPage.css";
 
-const TrackingPage = () => {
+const TrackingPage = ({ cart }) => {
   return (
     <>
-      <Header />
+      <Header cart={cart} />
       <div className="tracking-page">
         <div className="order-tracking">
           <Link className="back-to-orders-link link-primary" to="/orders">
